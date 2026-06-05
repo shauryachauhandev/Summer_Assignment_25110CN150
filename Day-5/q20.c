69d7fc3b70b0d5a6f68dab62bcfef26c1cd70c9c
shauryachauhandev <2110cn150@glbitm.ac.in>
2026-06-05 21:10:03 +0530

    Day-5 uploaded

--- Day-5/q20.c
+++ Day-5/q20.c
@@ -1,16 +1,27 @@
 #include<stdio.h>
 int main()
 {
-int n,i;
-printf("Enter the number whose factors you want:");
+int n,i,j,largest=0;
+printf("Enter the number:");
 scanf("%d",&n);
-printf("The factors of the number\n");
 for(i=1;i<=n;i++)
 {
 if(n%i==0)
 {
-printf("%d ",i);
+int count=0;
+for(j=1;j<=i;j++)
+{
+if(i%j==0)
+{
+count++;
 }
 }
+if(count==2)
+{
+largest=i;
+}
+}
+}
+printf("Largest prime factor=%d",largest);
 return 0;
 }
